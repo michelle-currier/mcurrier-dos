109492d07c8ecc80b218777aad864f798b908cbf
michelle-currier <mushel@gmail.com>
2025-05-22 13:12:23 -0500

    trying something new with the from

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: NextRequest) {
     // Create the email options
     const emailData = {
       to: "mcurrierdesigns@gmail.com", // Your email where you want to receive messages
-      from: `design.mcurrier.com`, // Sender's email
+      from: `${email}`, // Sender's email
       subject: `Contact inquiry from ${name}`,
       text: message,
       html: `
